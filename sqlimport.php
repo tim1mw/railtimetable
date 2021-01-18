0f9098001bab2f7abe92445c92b5cddbcc57c6d2
Tim Williams <tmw@autotrain.org>
2021-01-18 11:35:50 +0000

    Fix an issue setting up AUTOINCREMENT and indexes

--- sqlimport.php
+++ sqlimport.php
@@ -6,113 +6,70 @@ $sql = array();
 
 $sql[] = "
 CREATE TABLE {$wpdb->prefix}railtimetable_dates (
-  id int(11) NOT NULL,
+  id int(11) NOT NULL AUTO_INCREMENT,
   timetableid int(11) NOT NULL,
-  date date NOT NULL
+  date date NOT NULL,
+  PRIMARY KEY (id),
+  KEY date_index (date)
 );
 ".$charset_collate;
 
 $sql[] = "
-ALTER TABLE {$wpdb->prefix}railtimetable_dates
-  ADD PRIMARY KEY (id),
-  ADD KEY date (date);
-";
-
-$sql[] = "
-ALTER TABLE {$wpdb->prefix}railtimetable_dates
-  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-";
-
-$sql[] = "
 CREATE TABLE {$wpdb->prefix}railtimetable_eventdays (
-  id int(11) NOT NULL,
+  id int(11) NOT NULL AUTO_INCREMENT,
   date date NOT NULL,
-  event int(11) NOT NULL
-)
+  event int(11) NOT NULL,
+  PRIMARY KEY (id),
+  KEY event_index (date)
+);
 ".$charset_collate;
 
 $sql[] = "
-ALTER TABLE {$wpdb->prefix}railtimetable_eventdays
-  ADD PRIMARY KEY (id),
-  ADD KEY event_index (date);
-";
-
-$sql[] = "
-ALTER TABLE {$wpdb->prefix}railtimetable_eventdays
-  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
-";
-
-$sql[] = "
 CREATE TABLE {$wpdb->prefix}railtimetable_eventdetails (
-  id int(11) NOT NULL,
+  id int(11) NOT NULL AUTO_INCREMENT,
   title varchar(100) NOT NULL,
   description text NOT NULL,
   page int(20) NOT NULL,
   link text NOT NULL,
   buylink text NOT NULL,
   background varchar(6) NOT NULL,
-  colour varchar(6) NOT NULL
-)
+  colour varchar(6) NOT NULL,
+  PRIMARY KEY (id)
+);
 ".$charset_collate;
 
 $sql[] = "
-ALTER TABLE {$wpdb->prefix}railtimetable_eventdetails
-  ADD PRIMARY KEY (id);
-";
-
-$sql[] = "
-ALTER TABLE {$wpdb->prefix}railtimetable_eventdetails
-  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
-";
-
-$sql[] = "
 CREATE TABLE {$wpdb->prefix}railtimetable_stations (
-  id int(11) NOT NULL,
+  id int(11) NOT NULL AUTO_INCREMENT,
   name varchar(50) NOT NULL,
   description varchar(255) NOT NULL,
   sequence int(2) NOT NULL,
   requeststop int(11) NOT NULL,
   closed int(11) NOT NULL,
-  hidden int(11) NOT NULL
-)
+  hidden int(11) NOT NULL,
+  PRIMARY KEY (id),
+  KEY sequence (sequence)
+);
 ".$charset_collate;
 
 $sql[] = "
-ALTER TABLE {$wpdb->prefix}railtimetable_stations
-  ADD PRIMARY KEY (id);
-";
-
-$sql[] = "
-ALTER TABLE {$wpdb->prefix}railtimetable_stations
-  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
-";
-
-$sql[] = "
 CREATE TABLE {$wpdb->prefix}railtimetable_stntimes (
-  id int(11) NOT NULL,
+  id int(11) NOT NULL AUTO_INCREMENT,
   station tinyint(4) NOT NULL,
   timetableid int(11) NOT NULL,
   down_deps text NOT NULL,
   down_arrs text NOT NULL,
   up_deps text NOT NULL,
-  up_arrs text NOT NULL
-)
+  up_arrs text NOT NULL,
+  PRIMARY KEY (id),
+  KEY timetable (timetableid)
+);
 ".$charset_collate;
 
-$sql[] = "
-ALTER TABLE {$wpdb->prefix}railtimetable_stntimes
-  ADD PRIMARY KEY (id);
-";
-
-$sql[] = "
-ALTER TABLE {$wpdb->prefix}railtimetable_stntimes
-  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
-";
 
 $sql[] = "
 CREATE TABLE {$wpdb->prefix}railtimetable_timetables (
-  id int(11) NOT NULL,
+  id int(11) NOT NULL AUTO_INCREMENT,
   timetable varchar(8) NOT NULL,
   background varchar(6) NOT NULL,
   colour varchar(6) NOT NULL,
@@ -120,17 +77,11 @@ CREATE TABLE {$wpdb->prefix}railtimetable_timetables (
   totaltrains tinyint(4) NOT NULL,
   colsmeta text NOT NULL,
   buylink varchar(255) NOT NULL,
-  hidden int(1) NOT NULL
-)
+  hidden int(1) NOT NULL,
+  PRIMARY KEY (id),
+  KEY timetable (timetable)
+);
 ".$charset_collate;
 
-$sql[] = "
-ALTER TABLE {$wpdb->prefix}railtimetable_timetables
-  ADD PRIMARY KEY (id);
-";
 
-$sql[] = "
-ALTER TABLE {$wpdb->prefix}railtimetable_timetables
-  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
-";
 
